b90fea62a6beebfef00b14089629e782ff9d6cd0
dredly <dredly@gmail.com>
2022-01-17 18:50:10 +0000

    Defining Error Template

--- app.js
+++ app.js
@@ -73,8 +73,9 @@ app.all('*', (req, res, next) => {
 })
 
 app.use((err, req, res, next) => {
-    const { statusCode = 500, message = "Something went wrong" } = err;
-    res.status(statusCode).send(message);
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = "Oh no, something went terribly wrong";
+    res.status(statusCode).render('error', { err });
 })
 
 app.listen(3000, () => {
