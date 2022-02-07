16779ffe560115075be624cb5d54272099e79df1
dredly <dredly@gmail.com>
2022-02-07 20:03:11 +0000

    Register Route Logic

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
 
 router.get('/register', (req, res) => {
     res.render('users/register');
 })
 
-router.post('/register', async (req, res) => {
-    res.send(req.body);
-})
+router.post('/register', catchAsync(async (req, res) => {
+    try {
+        const { email, username, password } = req.body;
+        const user = new User({ email, username });
+        const registeredUser = await User.register(user, password);
+        console.log(registeredUser);
+        req.flash("Welcome to YelpCamp");
+        res.redirect('/campgrounds');
+    } catch (err) {
+        req.flash('error', err.message);
+        res.redirect('register');
+    }
+}))
 
 module.exports = router;
