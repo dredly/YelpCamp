1f13a995c16ea6584a4fc4cc2cd30c9fe15bdd52
dredly <dredly@gmail.com>
2022-02-07 20:22:49 +0000

    Login routes

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
 
@@ -19,6 +20,15 @@ router.post('/register', catchAsync(async (req, res) => {
         req.flash('error', err.message);
         res.redirect('register');
     }
+}))
+
+router.get('/login', (req, res) => {
+    res.render('users/login');
+})
+
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), catchAsync(async (req, res) => {
+    req.flash('success', 'Welcome back!');
+    res.redirect('/campgrounds');
 }))
 
 module.exports = router;
