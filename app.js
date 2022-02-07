9d255b5e4850d03f37c15eb15872f39c9bbb293a
dredly <dredly@gmail.com>
2022-02-07 11:14:45 +0000

    Register Form

--- app.js
+++ app.js
@@ -9,8 +9,10 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
 const ExpressError = require('./utils/ExpressError');
-const campgrounds = require('./routes/campgrounds');
-const reviews = require('./routes/reviews');
+
+const userRoutes = require('./routes/users');
+const campgroundRoutes = require('./routes/campgrounds');
+const reviewRoutes = require('./routes/reviews');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp');
 
@@ -53,16 +55,9 @@ app.use((req, res, next) => {
     next();
 })
 
-app.get('/fakeUser', async (req, res) => {
-    const user = new User({
-        email: 'miguel@gmail.com', username: 'mig'
-    });
-    const newUser = await User.register(user, 'miguel123');
-    res.send(newUser);
-})
-
-app.use('/campgrounds', campgrounds);
-app.use('/campgrounds/:id/reviews', reviews);
+app.use('/', userRoutes);
+app.use('/campgrounds', campgroundRoutes);
+app.use('/campgrounds/:id/reviews', reviewRoutes);
 
 
 app.all('*', (req, res, next) => {
