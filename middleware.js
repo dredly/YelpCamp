8906fa452d8cd6fa591df841af76981906259629
dredly <dredly@gmail.com>
2022-02-09 18:14:24 +0000

    ReturnTo behaviour

--- middleware.js
+++ middleware.js
@@ -1,6 +1,6 @@
 module.exports.loginRequired = (req, res, next) => {
-    console.log('req.user ......', req.user);
     if (!req.isAuthenticated()) {
+        req.session.returnTo = req.originalUrl;
         req.flash('error', 'Thou shallt be logged in to view this page');
         return res.redirect('/login');
     }
