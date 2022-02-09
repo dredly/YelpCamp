8eaf65d4e1abc5e129ac0b1c31775af3d11d7bc6
dredly <dredly@gmail.com>
2022-02-09 17:51:17 +0000

    currentUser Helper

--- middleware.js
+++ middleware.js
@@ -1,4 +1,5 @@
 module.exports.loginRequired = (req, res, next) => {
+    console.log('req.user ......', req.user);
     if (!req.isAuthenticated()) {
         req.flash('error', 'Thou shallt be logged in to view this page');
         return res.redirect('/login');
