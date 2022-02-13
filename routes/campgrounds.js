1bb3acde60c8fb94e0e2ff339e7371ee56c3e7e0
dredly <dredly@gmail.com>
2022-02-13 23:44:09 +0000

    Restructuring routes

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,18 +4,17 @@ const catchAsync = require('../utils/catchAsync');
 const campgrounds = require("../controllers/campgrounds");
 const { loginRequired, validateCampground, isAuthor } = require('../middleware');
 
-router.get('/', catchAsync(campgrounds.index));
+router.route('/')
+    .get(catchAsync(campgrounds.index))
+    .post(loginRequired, validateCampground, catchAsync(campgrounds.create));
 
 router.get('/new', loginRequired, campgrounds.renderNewForm);
 
-router.post('/', loginRequired, validateCampground, catchAsync(campgrounds.create));
-
-router.get('/:id', catchAsync(campgrounds.show));
+router.route('/:id')
+    .get(catchAsync(campgrounds.show))
+    .put(loginRequired, isAuthor, validateCampground, catchAsync(campgrounds.update))
+    .delete(loginRequired, isAuthor, catchAsync(campgrounds.delete));
 
 router.get('/:id/edit', loginRequired, isAuthor, catchAsync(campgrounds.renderEditForm));
-
-router.put('/:id', loginRequired, isAuthor, validateCampground, catchAsync(campgrounds.update));
-
-router.delete('/:id', loginRequired, isAuthor, catchAsync(campgrounds.delete));
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -4,15 +4,14 @@ const passport = require('passport');
 const catchAsync = require('../utils/catchAsync');
 const users = require('../controllers/users');
 
-router.get('/register', users.renderRegisterForm);
+router.route('/register')
+    .get(users.renderRegisterForm)
+    .post(catchAsync(users.register));
 
-router.post('/register', catchAsync(users.register));
-
-router.get('/login', users.renderLoginForm);
-
-router.post('/login',
-    passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }),
-    catchAsync(users.login))
+router.route('/login')
+    .get(users.renderLoginForm)
+    .post(passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }),
+        catchAsync(users.login));
 
 router.get('/logout', users.logout);
 
