ea1ba47259b1c8600163d53f6f3ca9936a9c311f
dredly <dredly@gmail.com>
2022-02-16 13:29:38 +0000

    Deleting images backend

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,4 +1,5 @@
 const Campground = require('../models/campground');
+const { cloudinary } = require('../cloudinary');
 
 module.exports.index = async (req, res) => {
     const campgrounds = await Campground.find({});
@@ -46,10 +47,15 @@ module.exports.renderEditForm = async (req, res) => {
 
 module.exports.update = async (req, res) => {
     const { id } = req.params;
-    console.log(req.body);
     const authorisedCampground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     authorisedCampground.images.push(...req.files.map(f => ({ url: f.path, filename: f.filename })));
     await authorisedCampground.save();
+    if (req.body.deleteImages) {
+        for (let filename of req.body.deleteImages) {
+            await cloudinary.uploader.destroy(filename);
+        }
+        await authorisedCampground.updateOne({ $pull: { images: { filename: { $in: req.body.deleteImages } } } });
+    }
     req.flash('success', 'Successfully updated campground');
     res.redirect(`/campgrounds/${authorisedCampground._id}`);
 }
