aa5c83f5461beb4f0ab90e6a6a8bc871fbc1a970
dredly <dredly@gmail.com>
2022-02-18 18:12:14 +0000

    Customising mapbox popup

--- public/scripts/showPageMap.js
+++ public/scripts/showPageMap.js
@@ -3,10 +3,17 @@ const geometry = JSON.parse(campground).geometry;
 console.log(geometry);
 const map = new mapboxgl.Map({
     container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v11', // style URL
+    style: 'mapbox://styles/mapbox/satellite-streets-v11', // style URL
     center: geometry.coordinates, // starting position [lng, lat]
     zoom: 8 // starting zoom
 });
 const marker1 = new mapboxgl.Marker()
     .setLngLat(geometry.coordinates)
+    .setPopup(
+        new mapboxgl.Popup({ offset: 25 })
+            .setHTML(
+                `<h3>${JSON.parse(campground).title}</h3>
+                <p>${JSON.parse(campground).location}</p>`
+            )
+    )
     .addTo(map);
