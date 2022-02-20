bd9fb3572045984e874f51b987cf7e3cca42aa4d
dredly <dredly@gmail.com>
2022-02-20 14:59:46 +0000

    Sanitising HTML with JOI

--- app.js
+++ app.js
@@ -57,7 +57,6 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
-    console.log(req.query);
     res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
