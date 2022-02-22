2fa01c11b31eecd5f66ddf636a9c7be01e73cb8a
dredly <dredly@gmail.com>
2022-02-22 16:47:18 +0000

    Frontend validation to prevent 0 star reviews

--- public/scripts/extraStarValidation.js
+++ public/scripts/extraStarValidation.js
@@ -1,8 +1,9 @@
 const ratingForm = document.querySelector('#rating-form');
 const starsFeedback = document.querySelector('#stars-feedback');
+const zeroStars = document.querySelector('#no-rate');
 
 ratingForm.addEventListener('submit', function (event) {
-    if (true) {
+    if (zeroStars.checked) {
         event.preventDefault();
         event.stopPropagation();
         starsFeedback.innerText = 'Make sure you have selected a rating between 1 and 5 stars';
